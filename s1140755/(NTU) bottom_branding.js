fec204fe74aafb70ffedf802c48db801460ef740
kwoky <kwokchung.yew@gmail.com>
2016-04-26 11:35:07 +0800

    Create (NTU) bottom_branding.js
    Original SaaS Ops version

---
+++ s1140755/(NTU) bottom_branding.js
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>NTU Home | NTU Library | Locate</title>
+</head>
+
+<body>
+
+<div id="tailwrap">
+	<center>
+	<div id="bottomLinks">
+		<font face="Arial" size="1">&nbsp;</font><p><font face="Arial" size="1">&nbsp;
+		</font></p>
+		<p><font face="Arial" size="1">&nbsp; </font></p>
+		<p><font face="Arial" size="1">&nbsp;</font></p>
+		<p><font face="Arial" size="1">&nbsp; </font></p>
+		<p><font face="Arial" size="1">&nbsp;</font></p>
+		<p><font face="Arial" size="1">&nbsp; </font></p>
+		<p><font face="Arial" size="1">
+		<a target="_blank" href="http://www.ntu.edu.sg">NTU Home</a> |
+		<a target="_blank" href="http://www.ntu.edu.sg/library">NTU Library</a> 
+		|
+		<a target="_blank" href="http://www.ntu.edu.sg/library/about/location/Pages/default.aspx">
+		Locate Us</a> |
+		<a target="_blank" href="http://www.ntu.edu.sg/library/about/Pages/Contactus.aspx">
+		Ask Us</a> | <a target="_blank" href="mailto:library@ntu.edu.sg">
+		Feedback</a> |
+		<a target="_blank" href="http://www.ntu.edu.sg/Pages/disclaimer.aspx">
+		Disclaimer</a> </font></div>
+	<font face="Arial" size="1"><span class="style2">© 2011 Nanyang 
+	Technological University. All rights reserved.<br>
+&nbsp;</span></font><p><span class="style2">
+	<font face="Arial" size="1">Powered 
+	by EBSCO<i>host</i></font></span></p>
+	<p>&nbsp;</p>
+	</center></div>
+
+</body>
+
+</html>
