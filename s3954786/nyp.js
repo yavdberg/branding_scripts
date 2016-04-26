a3fdaffa1ddaec67660a27235324953d531e5390
kwoky <kwokchung.yew@gmail.com>
2016-04-26 11:23:20 +0800

    Create nyp.js
    For FTF migration

---
+++ s3954786/nyp.js
@@ -0,0 +1,13 @@
+<center>
+<a href="https://library.nyp.edu.sg/" target="_blank">NYP Library</a> |
+<a href="https://library.nyp.edu.sg" target="_blank">Library On the Go</a> |
+<a href="mailto:NYP_Library_RefDesk@nyp.edu.sg" target="_blank">Ask-A-Librarian</a> |
+<a href="http://www.nyp.edu.sg/others/copyright-and-disclaimer" target="_blank">Copyright and Disclaimer </a>
+<p>Copyright © 2015 NYP, Singapore. All rights reserved.</p>
+</center>
+
+<script type='text/javascript'>
+	if(window.location.href.indexOf("sdb=edspub") > -1) {
+		document.getElementsByClassName("customerLogo")[0].style.display = 'none';
+    }
+</script>
