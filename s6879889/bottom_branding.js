4abcdc07a6f791d15757ddaa101b81c79d2ff8e5
kwoky <kwokchung.yew@gmail.com>
2016-04-26 11:27:43 +0800

    Create bottom_branding.js
    Uses customisation framework
     - Add NLB noticebar

---
+++ s6879889/bottom_branding.js
@@ -0,0 +1,3 @@
+<script id="custom-eds" src="http://gss.ebscohost.com/kcyew/nlb/nlb_custom.js"></script>
+
+<script type="text/javascript" id="component" src="//eservice-stg.nlb.gov.sg/components/js/bundle.js?key=QT0mFDTADwbx"></script>
